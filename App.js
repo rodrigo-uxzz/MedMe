05b4bb4fcad2ad801fc5e2c0f7a2f180216f3b14
Nicole Fernandes <nicolefernandesdasilva80@gmail.com>
2026-04-08 17:40:07 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //rotas
+import Splash from './src/pages/Splash';
+import Login from './src/pages/Login';
+import Cadastro from './src/pages/Cadastro';
 import Home from './src/pages/home';
 import Glicemia from './src/pages/glicemia';
 import Agua from './src/pages/agua';
@@ -15,46 +18,41 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#66E4D6',
-            
-          },
-          headerTintColor: '#000',
-          headerTitleStyle: {
-            fontWeight: '500',
-            fontSize: 30,
-            color: '#fff',
-          },
-        }}
-      >
-        <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
+      <Stack.Navigator>
+        <Stack.Screen name="Splash" component={Splash}
+            options={{ headerShown: false,}}
+        />
+        <Stack.Screen name="Login" component={Login}
+          options={{ headerShown: false,}}
+        />
+        <Stack.Screen name="Cadastro" component={Cadastro}
+          options={{ headerShown: false,}}
+        />
+        <Stack.Screen name="Home" 
+
+        component={Home} 
+        options={{ 
+          title: "MedMe",
+          headerStyle: { backgroundColor: "#66E4D6" },
+          headerTintColor: "#fff",
+          
+        }} />
         <Stack.Screen
           name="Glicemia"
           component={Glicemia}
           options={{
-            title: 'GLICEMIA',
-            
-            headerStyle: {
-              backgroundColor: '#FFF',
-              borderBottomWidth: 0,
-            },
-            headerTitleStyle: {
-              justifyContent: 'center',
-              textAlign: 'center',
-              color: '#66E4D6',
-              fontWeight: '500',
-              fontSize: 30,
-            },
-            headerTintColor: '#66E4D6',
+              title: "Glicemia",
+              headerStyle: { backgroundColor: "#66E4D6" },
+              headerTintColor: "#fff",
           }}
         />
         <Stack.Screen 
           name="Agua" 
           component={Agua} 
           options={{
+            title: "Água",
+            headerStyle: { backgroundColor: "#66E4D6" },
+            headerTintColor: "#fff",
             
           }}
         />
